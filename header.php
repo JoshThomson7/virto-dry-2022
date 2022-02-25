e886440f5699ccc727a7c1b81c066b9d04d04313
Josh <joshthomson15@gmail.com>
2022-02-25 20:10:28 +0000

    Logo / header / footer / vars

--- header.php
+++ header.php
@@ -31,12 +31,12 @@
             <div class="pre__header">
                 <div class="max__width">
                     <ul class="contact">
-                        <li><i class="fa fa-phone"></i><a href="tel:01463782323">01463 78 23 23</a></li>
-                        <li><i class="fa fa-envelope"></i><a href="mailto:reception@fraseroptical.co.uk">Email Us</a></li>
+                        <li><i class="fa fa-phone"></i><a href="tel:0123456789">0123 456 789</a></li>
+                        <li><i class="fa fa-envelope"></i><a href="mailto:info@virtodry.com">Email Us</a></li>
                     </ul>
 
                     <div class="book_button">
-                        <a href="/book-appointment/" class="book">Book Appointment</a>
+                        <a href="/book-appointment/" class="book">Book</a>
                     </div>
                 </div>
             </div>
@@ -48,7 +48,7 @@
                     <div class="header_logo_menu">
                         <div class="logo">
                             <a href="<?php echo esc_url(home_url()); ?>" title="<?php bloginfo('name'); ?>">
-                                <img src="https://fraser-optical.twsbeta3.co.uk/wp-content/uploads/2021/07/fraser-logo-e1627306300113.jpeg" alt="<?php bloginfo('name'); ?>">
+                                <img src="<?php echo esc_url(get_stylesheet_directory_uri()); ?>/img/virto-dry-sized.png" alt="">
                             </a>
                         </div><!-- logo -->
                     </div>
