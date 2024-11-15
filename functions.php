0a944d2dff366b0545e29440902a55abc65da28c
Josh Thomson <joshthomson15@gmail.com>
2024-11-15 00:09:38 +0000

    Woocommerce

--- functions.php
+++ functions.php
@@ -13,11 +13,16 @@ include('modules/wp-search-acf.php');
 include('modules/dashboard/dashboard-functions.php');
 include('modules/flexible-content/functions/fc-functions.php');
 // include('modules/gravity-forms/functions/gf-functions.php');
-include('modules/blog/functions/blog-functions.php');
+// include('modules/blog/functions/blog-functions.php');
 
 include('modules/off-canvas-nav/functions/oc-functions.php');
 include('modules/advanced-video-banners/functions/avb-functions.php');
 include('modules/advanced-team-members/functions/ateam-functions.php');
 include('modules/advanced-slide-menu/functions/asm-functions.php');
 include('modules/advanced-testimonials/functions/at-functions.php');
+
+add_action( 'after_setup_theme', 'yourtheme_woocommerce_support' );
+function yourtheme_woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
 ?>
